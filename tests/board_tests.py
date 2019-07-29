b20d36f4a0a7ea408e94ee323782d0a491d3c983
Rita <rita@sandas.lt>
2019-07-29 09:17:46 +0300

    Testing

--- tests/board_tests.py
+++ tests/board_tests.py
@@ -1,17 +1,19 @@
 import unittest
-#from . import field
 
+import sys
+sys.path.append("..")
+import field
 
 
 class TestBoard(unittest.TestCase):
 
     def test_test_Field(self):
 
-        #f1 = field.Field(1, 4, False)
+        f1 = field.Field(1, 4, False)
 
-        #f1.setBomb()
-        #self.assertTrue(f1.isBomb)
-        self.assertTrue(True)
+        f1.setBomb()
+        self.assertTrue(f1.isBomb)
+        #self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
