cf6a1045f9722a6d512fcd18c3dfe54b6c9a78cd
Rita <rita@sandas.lt>
2019-07-29 09:20:36 +0300

    Testing

--- run.py
+++ run.py
@@ -1,24 +1,19 @@
 #export FLASK_APP=run.py
-#ccs
-#klase
-#responsive
-#.body {
-#    display: flex;
-#    flex-wrap: wrap;
-#}
+#https://realpython.com/the-ultimate-flask-front-end/
 
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class Run:
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/more')
-def more():
-    return render_template('more.html')
+    @app.route('/more')
+    def more():
+        return render_template('more.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
