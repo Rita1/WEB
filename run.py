80e3b1e9cc2d6a3a4cf4ddf0c7c667fac6ea0b49
Rita <rita@sandas.lt>
2019-08-09 09:42:26 +0300

    Test

--- run.py
+++ run.py
@@ -1,19 +1,35 @@
-#export FLASK_APP=run.py
-#https://realpython.com/the-ultimate-flask-front-end/
+# source env/bin/activate
+# export FLASK_APP=run.py
+# flask run
+# pip freeze > requirements.txt
+# npm run watch
+# npm install jquery --save
+#  https://realpython.com/the-ultimate-flask-front-end/
 
-from flask import Flask
-from flask import render_template
+import json
+from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__,  template_folder='./static')
+app = Flask(__name__, static_url_path='/static', template_folder='./static')
 
-class Run:
+
+class Server():
+
     @app.route('/')
     def index():
         return render_template('index.html')
 
-    @app.route('/more')
-    def more():
-        return render_template('more.html')
+    @app.route('/board')
+    def getGame():
+        my_dict = {
+            1 : "A",
+            2 : "B",
+            3 : "C",
+        }
 
+        json_dict = json.dumps(my_dict)
+        return json_dict
+
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
