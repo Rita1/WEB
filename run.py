951c39666722a4707aa9d73341dcd5fbff150bb2
Rita <rita@sandas.lt>
2019-08-14 08:21:29 +0300

    Pakeitimai

--- run.py
+++ run.py
@@ -7,7 +7,7 @@
 #  https://realpython.com/the-ultimate-flask-front-end/
 
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # app = Flask(__name__,  template_folder='./static')
 app = Flask(__name__, static_url_path='/static', template_folder='./static')
@@ -18,16 +18,41 @@ class Server():
     @app.route('/')
     def index():
         return render_template('index.html')
-
+    
+    # Board : sizeX, sizeY, listof Fields
+    # Size: Small 9 x 9, Medium 16 x 16, Large 30 x 24
+    # Field : cordX, cordY, condition (Flag, DUG, Untouch), isBomb, BombCount
     @app.route('/board')
     def getGame():
-        my_dict = {
-            1 : "A",
-            2 : "B",
-            3 : "C",
-        }
 
+        my_dict = {
+            "userCount" : 2,
+            "board" : {
+                "cordX" : 9,
+                "cordY" : 9,
+                "fieldList" : {
+                    0 : {
+                        "cordX" : 0,
+                        "cordY" : 0,
+                        "condition" : "Untouch",
+                        "isBomb" : False,
+                        "bombCount" : 0
+                    },
+                    1 : {
+                        "cordX" : 1,
+                        "cordY" : 0,
+                        "condition" : "Untouch",
+                        "isBomb" : False,
+                        "bombCount" : 0
+                    }
+                }
+            },
+        }
+        username = request.args.get('userName')
+        size = request.args.get('size')
+        print("username", username, "size", size)
         json_dict = json.dumps(my_dict)
+        print("json_dict", json_dict)
         return json_dict
 
         
