48bf3f9dc9d5685edfbaf6951f6730c374aaa585
Rita <rita@sandas.lt>
2019-11-17 22:44:39 +0200

    changes

---
+++ tests/test_user.py
@@ -0,0 +1,26 @@
+import unittest
+import pytest
+
+from .. import field
+from .. import board
+from .. import run
+from .. import user
+
+
+class TestUser(unittest.TestCase):
+
+    def test_User(self):
+
+        u1 = user.User("Jhon", 1000)
+        u1Dict = u1.get_info()
+        self.assertEqual(u1Dict["username"], "Jhon")
+        self.assertEqual(u1Dict["cookie"], 1000)
+        self.assertEqual(u1Dict["flaged_qty"], 0)
+        self.assertEqual(u1Dict["digged_qty"], 0)
+
+        u1.increase_digged(2)
+        u1.increase_flag(5)
+        u1Dict = u1.get_info()
+        self.assertEqual(u1Dict["flaged_qty"], 5)
+        self.assertEqual(u1Dict["digged_qty"], 2)
+        self.assertEqual(u1.return_cookie(), 1000)
---
+++ user.py
@@ -0,0 +1,46 @@
+import time
+from datetime import datetime, timedelta
+
+class User():
+
+    # name - prisiregistravusio zaidejo vardas
+    # cookie
+    # timestamp - kada useris prisiregistravo, senus ismetam lauk
+    # flaged_qty - Veliavele pazymeta tasku
+    # digged_qty -  Atidengta lauku su taskais
+    # total_qty - Bendras tasku kiekis
+
+    def __init__(self, name, cookie):
+        self.name = name
+        self.cookie = cookie
+        self.timestamp = datetime.now().timestamp()
+        self.flaged_qty = 0
+        self.digged_qty = 0
+
+    def increase_flag(self, qty):
+        self.flaged_qty += qty
+
+    def increase_digged(self, qty):
+        self.digged_qty += qty
+
+    def return_cookie(self):
+        return self.cookie
+
+    def return_timestamp(self):
+        return self.timestamp
+    
+    # returns dict about user:
+        #        {
+        #          "username" : "Petras",
+        #          "flaged_qty" : 0,
+        #          "digged_qty " : 0,
+        #          "total_qty" : 0,
+        #        }
+    def get_info(self):
+        user = {}
+        user["username"] = self.name
+        user["cookie"] = self.cookie
+        user["flaged_qty"] = self.flaged_qty
+        user["digged_qty"] = self.digged_qty
+
+        return user
